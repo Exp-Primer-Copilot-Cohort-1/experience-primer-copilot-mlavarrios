7661a3000973ebd3e3ff44a318bf28b6ce4e64b6
MarcoLavarrios <37081215+mlavarrios@users.noreply.github.com>
2023-11-01 21:29:16 +0000

    Fourth commit

--- comments.js
+++ comments.js
@@ -1,80 +1,47 @@
-// Create web servervar express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Comment = mongoose.model('Comment');
-var User = mongoose.model('User');
-var jwt = require('express-jwt');
-var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
-// var auth = jwt({secret: process.env.JWT_SECRET, userProperty: 'payload'});
+//Create web server in javascript
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var ROOT_DIR = "html/";
+var comments = [];
 
-// GET /comments
-// Route for comments collection
-router.get('/', function(req, res, next) {
-  Comment.find(function(err, comments) {
-    if (err) {
-      return next(err);
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true, false);
+    if (urlObj.pathname === "/comment") {
+        console.log("comment route");
+        if (req.method === "POST") {
+            console.log("POST comment route");
+            var jsonData = "";
+            req.on('data', function (chunk) {
+                jsonData += chunk;
+            });
+            req.on('end', function () {
+                var reqObj = JSON.parse(jsonData);
+                console.log(reqObj);
+                console.log("Name: " + reqObj.Name);
+                console.log("Comment: " + reqObj.Comment);
+                comments.push(reqObj);
+                console.log(comments);
+                res.writeHead(200);
+                res.end("");
+            });
+        }
+        else if (req.method === "GET") {
+            console.log("In GET");
+            res.writeHead(200);
+            var data = JSON.stringify(comments);
+            res.end(data);
+        }
     }
-    res.json(comments);
-  });
-});
-
-// POST /comments
-// Route for creating comments
-router.post('/', auth, function(req, res, next) {
-  // Create a new comment object
-  var comment = new Comment(req.body);
-  comment.author = req.payload.username;
-  // Save the comment object to the database
-  comment.save(function(err, comment) {
-    if (err) {
-      return next(err);
+    else {
+        fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
     }
-    res.json(comment);
-  });
-});
-
-// Preload comment objects on routes with ':comment'
-router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id);
-
-  query.exec(function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    if (!comment) {
-      return next(new Error('can\'t find comment'));
-    }
-    req.comment = comment;
-    return next();
-  });
-});
-
-// GET /comments/:comment
-// Route for specific comments
-router.get('/:comment', function(req, res) {
-  res.json(req.comment);
-});
-
-// PUT /comments/:comment/upvote
-// Route for upvoting comments
-router.put('/:comment/upvote', auth, function(req, res, next) {
-  req.comment.upvote(function(err, comment) {
-    if (err) {
-      return next(err);
-    }
-    res.json(comment);
-  });
-});
-
-// POST /comments/:comment/replies
-// Route for creating replies
-router.post('/:comment/replies', auth, function(req, res, next) {
-  var reply = new Comment(req.body);
-  reply.author = req.payload.username;
-  reply.upvotes = 0;
-  reply.save(function(err, reply) {
-    if (err) {
-      return next(err);
-    }
-    req.comment.replies.push(reply);
-    req.comment
+}).listen(80);
